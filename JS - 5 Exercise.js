8bbdd2d2f16c5236b281b57e7f9b2355a52a0d27
Matan Roytman <89573242+MatanRoytman@users.noreply.github.com>
2021-08-30 17:46:14 +0300

    excercise 3
    The range of the grades from the second half of the school year

--- JS - 5 Exercise.js
+++ JS - 5 Exercise.js
@@ -1,8 +1,22 @@
-
-const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92]
+//----------------------   excercise 1 ----------------------------------//
+const grades = [73, 91, 88, 56, 62, 84, 95, 96, 88, 92];
 const difference = (grades = []) => {
    const highest = Math.max(...grades);
    const lowest = Math.min(...grades);
    return highest - lowest;
 };
-console.log(difference(grades));
+console.log("Range: " + difference(grades));
+
+//----------------------   excercise 2 ----------------------------------//
+grades.sort();
+console.log("Median:" + grades[5]);
+
+//----------------------   excercise 3 ----------------------------------//
+var leftSide = grades.slice(5,10);
+console.log(leftSide);
+const halfDifference = (leftSide = []) => {
+    const highest = Math.max(...leftSide);
+    const lowest = Math.min(...leftSide);
+    return highest - lowest;
+ };
+ console.log("Half Range: " + halfDifference(leftSide));
